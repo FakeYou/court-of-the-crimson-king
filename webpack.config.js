dc427d15cd0affe52397b6d81584a382f368350a
Andre Nanninga <ag.nanninga@gmail.com>
2018-02-04 08:18:58 +0100

    working 'production' config

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,23 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const PUBLIC_PATH = process.env.PUBLIC_PATH || '/';
+console.log(PUBLIC_PATH);
+
 module.exports = {
 	devtool: 'inline-source-map',
 	entry: [
 		'./src/index'
 	],
 	plugins: [
+		new webpack.DefinePlugin({
+			'process.env.PUBLIC_PATH': JSON.stringify(PUBLIC_PATH)
+		}),
 		new webpack.NamedModulesPlugin(),
 		new webpack.HotModuleReplacementPlugin(),
 		new HtmlWebpackPlugin({
 			title: 'Court of the Crimson King',
-			hash: true
+			hash: true,
 		})
 	],
 	devServer: {
@@ -25,7 +31,7 @@ module.exports = {
 	},
 	output: {
 		path: path.join(__dirname, './docs'),
-		publicPath: '/',
 		filename: 'game.bundle.js',
+		publicPath: PUBLIC_PATH,
 	},
 };
